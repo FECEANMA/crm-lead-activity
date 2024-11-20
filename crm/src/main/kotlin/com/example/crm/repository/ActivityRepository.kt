7137e0a15f8d5fc5168f7f57cee6e82f96d40fb8
Fernando <146852179+FECEANMA@users.noreply.github.com>
2024-11-19 21:53:26 -0500

    Delete crm directory

--- crm/src/main/kotlin/com/example/crm/repository/ActivityRepository.kt
+++
@@ -1,9 +0,0 @@
-package com.example.crm.repository
-
-import com.example.crm.entity.Activity
-import org.springframework.data.jpa.repository.JpaRepository
-import org.springframework.stereotype.Repository
-
-@Repository
-interface ActivityRepository: JpaRepository<Activity,Long> {
-}
--- crm/src/main/resources/db/migration/V1__create_customer.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE IF NOT EXISTS customer (
-id SERIAL,
-full_name VARCHAR(255) NOT NULL,
-email VARCHAR(255) NOT NULL,
-phone VARCHAR(255),
-address VARCHAR(255),
-PRIMARY KEY (id)
-);
